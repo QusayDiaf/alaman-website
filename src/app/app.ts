457bdd57f0026f32f51a039bd15f7c77a8dddc0d
qusay_diaf <kosayking15@gmail.com>
2026-06-13 19:24:43 +0200

    add features

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal ,HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('alaman-front');
+  mouseX = 0;
+  mouseY = 0;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    // طرح 10 بكسل باش يجي الماوس في منتصف الدائرة بالضبط (نصف قطر الدائرة)
+    this.mouseX = event.clientX - 5;
+    this.mouseY = event.clientY - 5;
+  }
 }
